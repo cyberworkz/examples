763f8a2274ab95c8c5653ab9ae3b1ac60fbfa396
Haiko van der Schaaf <hvdschaaf@gmail.com>
2022-03-03 22:37:57 +0100

    add bookrepo to providers

--- online-library/src/app.module.ts
+++ online-library/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
+import { BooksRepository } from './books/books.repository';
 import { BooksService } from './books/books.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, BookController, BooksController],
-  providers: [AppService, BooksService],
+  controllers: [AppController, BooksController, BooksController],
+  providers: [AppService, BooksService, BooksRepository],
 })
 export class AppModule {}
