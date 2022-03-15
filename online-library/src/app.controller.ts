52490b8fb2e2d9041696cfc6f4d5d9c636a7ae43
haiko van der schaaf <haiko.van.der.schaaf@ritense.com>
2022-03-15 16:48:51 +0100

    return ping

--- online-library/src/app.controller.ts
+++ online-library/src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
-  getHello(): string {
+  @Get('ping')
+  getPing(): string {
     return this.appService.getHello();
   }
 }
