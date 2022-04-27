880df0ba36d38492012312e3c603559a909ded03
haiko van der schaaf <haiko.van.der.schaaf@ritense.com>
2022-04-27 19:42:31 +0200

    add exception logging online-library service

--- online-library/src/app.module.ts
+++ online-library/src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
 import { BooksRepository } from './books/books.repository';
 import { BooksService } from './books/books.service';
+import { ExceptionsLoggerFilter } from './utils/ExceptionsLogFilter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController, BooksController],
-  providers: [AppService, BooksService, BooksRepository],
+  providers: [AppService, BooksService, BooksRepository,
+   { provide: APP_FILTER,
+    useClass: ExceptionsLoggerFilter,
+   }
+  ],
 })
 export class AppModule {}
