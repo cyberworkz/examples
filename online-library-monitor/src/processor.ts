79003710218cdc54e8fc00d22eb9269818418046
haiko van der schaaf <hvdschaaf@gmail.com>
2022-04-28 21:46:18 +0200

    message slack

--- online-library-monitor/src/processor.ts
+++ online-library-monitor/src/processor.ts
@@ -1,3 +1,4 @@
+import { ErrorCode, WebClient } from '@slack/web-api';
 import {
   CloudWatchLogsDecodedData,
   CloudWatchLogsEvent,
@@ -6,6 +7,9 @@ import {
 } from 'aws-lambda';
 import * as zlib from "zlib";
 
+// Read a token from the environment variables
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
 
 /**
  * Note: Cloudwatch log events are gzip-compressed and base64-encoded
@@ -21,6 +25,29 @@ const processor: CloudWatchLogsHandler = async (event: CloudWatchLogsEvent) => {
     const decodedData: CloudWatchLogsDecodedData = JSON.parse(zlib.unzipSync(payload).toString()) as CloudWatchLogsDecodedData;
 
     console.log("Log group:" + decodedData.logGroup);
+
+    for (const logEvent of decodedData.logEvents) {
+    
+      console.log(decodedData.logGroup + " " + logEvent.message);
+
+      // message Slack
+      try {
+        const result  = await web.chat.postMessage({
+          text: logEvent.message,
+          channel: '#general'
+        });
+        console.log(result);
+      }
+      catch (error) {
+        // Check the code property, and when its a PlatformError, log the whole response.
+        if (error.code === ErrorCode.PlatformError) {
+          console.log(error.data);
+        } else {
+          // Some other error, oh no!
+          console.log('Well, that was unexpected.');
+        }
+      }
+    }
   }
 };
 
