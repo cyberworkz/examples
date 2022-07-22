76f9ba58ea4895fdd6ad275eb037790129a30919
Haiko van der Schaaf <hvdschaaf@gmail.com>
2022-07-22 23:59:30 +0200

    add API endpoints and template.yml

---
+++ online-library-java/src/main/java/com/serverlesscorner/onlinelibrary/Book.java
@@ -0,0 +1,55 @@
+package com.serverlesscorner.onlinelibrary;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Integer isbn;
+
+    private String title;
+
+    private Author author;
+
+    public Book(Integer isbn, String title, Author author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+}
--- online-library-java/src/main/java/com/serverlesscorner/onlinelibrary/OnlineLibraryController.java
+++ online-library-java/src/main/java/com/serverlesscorner/onlinelibrary/OnlineLibraryController.java
@@ -3,11 +3,20 @@ package com.serverlesscorner.onlinelibrary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.List;
+
+
 @RestController
 @EnableWebMvc
+@RequestMapping(value = "/books")
 @Import(OnlineLibraryService.class)
 public class OnlineLibraryController {
 
@@ -19,7 +28,23 @@ public class OnlineLibraryController {
         this.service = service;
     }
 
-    public Object getBookByIsbn(Integer isbn){
-        service.getBookByIsbn(isbn);
+    @GetMapping(value = "/{isbn}", produces = "application/json")
+    public ResponseEntity<Book> getBookByIsbn(@PathVariable("isbn") Integer isbn){
+        Book book = service.getBookByIsbn(isbn);
+
+        if(book != null){
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } else {
+            LOG.info(" book not found");
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/author/{lastName}/{firstName}")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@PathVariable("lastName") String lastName, @PathVariable("firstName") String firstName) {
+        List<Book> booksFromAuthor = service.getBooksFromAuthor(new Author(firstName, lastName));
+
+        return new ResponseEntity<>(booksFromAuthor, HttpStatus.OK);
     }
 }
