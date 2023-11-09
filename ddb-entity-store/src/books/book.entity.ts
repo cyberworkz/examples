25c1fe82a9722b3bbc2260a2e1008677aeacb2bd
haiko van der schaaf <hvdschaaf@gmail.com>
2023-11-09 16:42:28 +0100

    fix tslint errors

--- ddb-entity-store/src/books/book.entity.ts
+++ ddb-entity-store/src/books/book.entity.ts
@@ -1,15 +1,15 @@
-import { DynamoDBValues, createEntity } from "@symphoniacloud/dynamodb-entity-store";
-import { IBook } from "./ibook";
+import { DynamoDBValues, createEntity } from '@symphoniacloud/dynamodb-entity-store';
+import { IBook } from './ibook';
 
-const isBook = (x:DynamoDBValues): x is IBook {
-    const candidate = x as IBook
-    return candidate.author.firstName !== undefined && candidate.author.lastName !== undefined 
-    && candidate.isbn !== undefined && candidate.title !== undefined
-}
+const isBook = (x: DynamoDBValues): x is IBook {
+    const candidate = x as IBook;
+    return candidate.author.firstName !== undefined && candidate.author.lastName !== undefined
+    && candidate.isbn !== undefined && candidate.title !== undefined;
+};
 
 export const  BOOK_ENTITY = createEntity(
     'BOOK',
     isBook,
     ({isbn}: Pick<IBook, 'isbn'>) => 'BOOK#${isbn}',
-    ({isbn}: Pick<IBook, 'isbn'>) => 'BOOK#${isbn}'
-)
+    ({isbn}: Pick<IBook, 'isbn'>) => 'BOOK#${isbn}',
+);
